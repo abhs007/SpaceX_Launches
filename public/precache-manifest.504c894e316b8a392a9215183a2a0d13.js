80f9e3af0f19985d1a0cf166ae29c19682cfb0ca
Vaishnav Abhishek <Abhishek.Vaishnav1@T-Mobile.com>
2020-09-13 20:51:40 -0700

    Completed React Apollo GraphQL

---
+++ public/precache-manifest.504c894e316b8a392a9215183a2a0d13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea284a6954d696e4bf978e277b811ee6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82661fe2f683bbb5ef3b",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "4dfd61f8b1834a4fa333",
+    "url": "/static/js/2.7968bbaa.chunk.js"
+  },
+  {
+    "revision": "83e895f9a437e69d1a80f15ce6e50621",
+    "url": "/static/js/2.7968bbaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82661fe2f683bbb5ef3b",
+    "url": "/static/js/main.df4117c0.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "18adf3db4014d7e36e4e3f90f9b35916",
+    "url": "/static/media/logo.18adf3db.png"
+  }
+]);
